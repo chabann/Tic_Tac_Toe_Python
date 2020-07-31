d717f4f156199ff3d65c3055307d8f9e5ea03c2e
Anna <32645521+chabann@users.noreply.github.com>
2020-07-31 23:17:58 +0300

    Add files via upload

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -4,19 +4,21 @@ from tkinter import *
 window = Tk()
 
 window.title("Tic Tac Toe")
-window.geometry("310x450")
+window.geometry("310x700")
 window.resizable(width=False, height=False)
 
 width_sq = 300
 height_sq = 260
 
 Menu_frame = Frame(window, bd=0, cursor="arrow")
-Play_frame = Frame(window, width=width_sq, height=height_sq, bd=1, cursor="circle")
-Result_frame = Frame(window, bd=3, cursor="arrow")
+Play_frame = Frame(window, bd=2, cursor="circle", bg="green")
+Result_frame = Frame(window, bd=3, height=10, cursor="arrow", bg="red")
+New_frame = Frame(window, bd=1, cursor="arrow", bg="blue")
 
 Menu_frame.pack()
 Play_frame.pack()
 Result_frame.pack()
+New_frame.pack()
 
 cnv = Canvas(Play_frame)
 cnv.pack(fill=BOTH, expand=1)
@@ -24,9 +26,8 @@ cnv.create_rectangle(0, 0, width_sq, height_sq, fill="#FFE4B5")
 
 result = Canvas(Result_frame)
 result.pack(fill=BOTH)
-result.create_rectangle(0, 0, 300, 100, fill="#F0E68C")
-result.create_text(150, 50, text="", justify=CENTER)
-
+result.create_rectangle(0, 0, 300, 80, fill="#F0E68C")
+result.create_text(150, 48, text="Aaaa", justify=CENTER)
 
 def WhoFirst():
     move = random.choice([1, 0])
@@ -62,28 +63,82 @@ def MakeDecision(event):
         cornery2 = height_sq
     type_man = type_sym.get()
     comp_choice = random.choice(free)
-    Number_Coords(comp_choice)
+    cx1, cy1, cx2, cy2 = Number_Coords(comp_choice)
 
     if type_man == "Cross":
-        Draw_cross(cornerx, cornery, cornerx2, cornery2)
+        Draw_cross(cornerx+5, cornery+5, cornerx2-5, cornery2-5)
+        Draw_zero(cx1 + 5, cy1 + 5, cx2 - 5, cy2 - 5)
     else:
-        Draw_zero(cornerx, cornery, cornerx2, cornery2)
+        Draw_zero(cornerx+5, cornery+5, cornerx2-5, cornery2-5)
+        Draw_cross(cx1 + 5, cy1 + 5, cx2 - 5, cy2 - 5)
 
 
 def Draw_cross(x1, y1, x2, y2):
-    cnv.create_line(x1, y1, x2, y2)
-    cnv.create_line(x2, y1, x1, y2)
+    cnv.create_line(x1, y1, x2, y2, width=3, fill="#DC143C")
+    cnv.create_line(x2, y1, x1, y2, width=3, fill="#DC143C")
 
 
 def Draw_zero(x1, y1, x2, y2):
-    cnv.create_oval(x1, y1, x2, y2)
+    cnv.create_oval(x1, y1, x2, y2, width=3, outline="#DC143C")
+
 
 def Number_Coords(n):
-    return 1
+    global free, x1, x2, y1, y2
+    c1, c2, c3, c4 = 0, 0, 0, 0
+    if n == 1:
+        c1 = 0
+        c2 = 0
+        c3 = x1
+        c4 = y1
+    elif n == 2:
+        c1 = x1
+        c2 = 0
+        c3 = x2
+        c4 = y1
+    elif n == 3:
+        c1 = x2
+        c2 = 0
+        c3 = width_sq
+        c4 = y1
+    elif n == 4:
+        c1 = 0
+        c2 = y1
+        c3 = x1
+        c4 = y2
+    elif n == 5:
+        c1 = x1
+        c2 = y1
+        c3 = x2
+        c4 = y2
+    elif n == 6:
+        c1 = x2
+        c2 = y1
+        c3 = width_sq
+        c4 = y2
+    elif n == 7:
+        c1 = 0
+        c2 = y2
+        c3 = x1
+        c4 = height_sq
+    elif n == 8:
+        c1 = x1
+        c2 = y2
+        c3 = x2
+        c4 = height_sq
+    elif n == 9:
+        c1 = x2
+        c2 = y2
+        c3 = width_sq
+        c4 = height_sq
+    return c1, c2, c3, c4
 
 
 def Coords_Number():
     return 1
+
+
+def new_game():
+    print(1)
 
 
 type_sym = StringVar()
@@ -97,6 +152,10 @@ lblzero = Label(Menu_frame, text="Zero")
 cross = Radiobutton(Menu_frame, variable=type_sym, value="Cross")
 zero = Radiobutton(Menu_frame, variable=type_sym, value="Zero")
 type_sym.set("Cross")
+
+
+New_game = Button(New_frame, text=" NEW GAME ", command=new_game)
+New_game.pack(side=TOP)
 
 free = [i for i in (1, 10)]
 x1 = width_sq / 3
